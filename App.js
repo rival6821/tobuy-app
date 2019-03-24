a857de3eb5d77d6231df70f706105390bdb9e8f2
ilhoon lee <rival6821@naver.com>
2019-03-24 22:19:22 +0900

    리스트 디자인

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar,TextInput, Dimensions, Platform } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform,
+  ScrollView
+} from 'react-native';
+import ToBuy from './ToBuy';
 
-const {height, width} = Dimensions.get("window");
+const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
+  state = {
+    newTodo: '',
+  }
   render() {
+    const { newTodo } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}>ToBuy List</Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"새로운 살것"}/>
+          <TextInput 
+            style={styles.input} 
+            placeholder={"새로운 살것"} 
+            value={newTodo} 
+            onChangeText={this._controlNewToDo} 
+            placeholderTextColor={'#999'}
+            returnKeyType={'done'}
+            autoCorrect={false}
+          />
+          <ScrollView>
+            <ToBuy />
+          </ScrollView>
         </View>
       </View>
     );
+  }
+  _controlNewToDo = text => {
+    this.setState({
+      newTodo: text
+    })
   }
 }
 
@@ -23,34 +53,38 @@ const styles = StyleSheet.create({
     backgroundColor: '#f23657',
     alignItems: 'center',
   },
-  title:{
-    color:"white",
-    fontSize:30,
-    marginTop:50,
-    fontWeight:"200",
-    marginBottom:30
+  title: {
+    color: "white",
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: "200",
+    marginBottom: 30
   },
-  card:{
-    backgroundColor : 'white',
-    flex:1,
-    width : width - 25,
-    borderTopLeftRadius : 10,
-    borderTopRightRadius : 10,
+  card: {
+    backgroundColor: 'white',
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
     ...Platform.select({
-      ios : {
-        shadowColor : 'rgb(50,50,50)',
-        shadowOpacity : 0.5,
-        shadowRadious : 5,
-        shadowOffset:{
-          height:-1
+      ios: {
+        shadowColor: 'rgb(50,50,50)',
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
         }
       },
-      android : {
-        
+      android: {
+        elevation: 3
       }
     })
   },
-  input : {
-
+  input: {
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 25,
   }
 });
---
+++ ToBuy.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacuty, StyleSheet } from 'react-native';
+
+class ToBuy extends Component {
+    render() {
+        return (
+            <View>
+                <Text>I am To Buy</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default ToBuy;
